233d882402c428abb1b2612a3671fecb33f615b1
Naresh <mekalanaresh225@achiever-108.local>
2018-12-27 18:46:27 +0530

    i am changed the Code

---
+++ versionController/versionController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  versionController
+//
+//  Created by Naresh on 12/27/18.
+//  Copyright © 2018 Naresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        naresh()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func naresh(){
+        print("naresh")
+    }
+
+
+}
+
